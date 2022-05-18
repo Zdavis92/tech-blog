f64ca719d3a7c72b2fe60d6c55d3b9a7634db6c2
Zach Davis <zacharydavis92@gmail.com>
2022-05-17 20:13:32 -0500

    'added deletePost function'

--- public/javascript/updatePost.js
+++ public/javascript/updatePost.js
@@ -1,7 +1,8 @@
+const postId = window.location.toString().split('/')[window.location.toString().split('/').length -1];
+
 async function updatePost() {
     const title = document.querySelector('#postTitle').value.trim();
     const body = document.querySelector('#postBody').value.trim();
-    const postId = window.location.toString().split('/')[window.location.toString().split('/').length -1];
 
     const response = await fetch(`/post/${postId}`, {
         method: 'put',
@@ -19,4 +20,15 @@ async function updatePost() {
     }
 }
 
+async function deletePost() {
+    const response = await fetch(`/post/${postId}`,{method: 'delete'})
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
 document.querySelector('#updatePost-btn').addEventListener('click', updatePost);
+document.querySelector('#deletePost-btn').addEventListener('click', deletePost);
